878bb98ce49287a1d64f1bc90e6eab715290e58a
Andrew Kennedy <akennedy811@icloud.com>
2022-12-06 21:03:00 -0500

    updated FindEvent query

--- routes/EventRouter.js
+++ routes/EventRouter.js
@@ -1,14 +1,15 @@
 const Router = require('express').Router()
 const controller = require('../controllers/EventController')
 
-// url: /api/event/?parameter=query
-Router.get('/', controller.FindEventsByDateAsc)
+// url: /api/event/?query=value
+Router.get('/', controller.FindEvents)
 
 // url: /api/event/:eventId
 Router.get('/:eventId', controller.GetEventById)
 Router.post('/:eventId', controller.CreateEvent)
 Router.put('/:eventId', controller.UpdateEvent)
 Router.delete('/:eventdId', controller.DeleteEvent)
+
 
 
 module.exports = Router
