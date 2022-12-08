440f17f8c1e035a41ac8d52112ba86f5ae691758
Andrew Kennedy <akennedy811@icloud.com>
2022-12-08 12:37:35 -0500

    added route protection

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -1,13 +1,15 @@
 const Router = require('express').Router()
 const controller = require('../controllers/CommentController')
+const auth = require('../middleware/auth')
+
 
 // url: /api/comment?params=query
 Router.get('/', controller.FindComments)
 
 // url: /api/comment/:commentId
 Router.get('/:eventId', controller.GetCommentByEvent) 
-Router.post('/', controller.CreateComment)
-Router.put('/:commentId', controller.UpdateComment)
-Router.delete('/:commentId', controller.DeleteComment)
+Router.post('/', auth.stripToken, auth.verifyToken, controller.CreateComment)
+Router.put('/:commentId', auth.stripToken, auth.verifyToken, controller.UpdateComment)
+Router.delete('/:commentId', auth.stripToken, auth.verifyToken, controller.DeleteComment)
 
 module.exports = Router
--- seeders/20221202044712-comments.js
+++ seeders/20221202044712-comments.js
@@ -5,21 +5,31 @@ const { User, Event, sequelize } = require('../models');
 module.exports = {
   async up (queryInterface, Sequelize) {
     const comments = await Promise.all(
-      [...Array(350)].map(async () => {
+      [...Array(400)].map(async () => {
       
         //PICK A RANDOM USER 
         let user = await User.findOne({ order: sequelize.random(), raw: true})
         //Pick a random event 
         let event = await Event.findOne({ order: sequelize.random(), raw: true})
         //pick a random phrase 
-        let phrases = [ 'Wow', 
-                        'so much fun', 
-                        'definitly not sponsered to comment on this event', 
-                        '7/10 too much water', 
-                        'saw too many birds for my liking',
-                        'too cold', 
-                        'organisers need to do a better job',
-                        'i like wearing shorts they easy and comfy to wear']
+        let phrases = [ 'Wow.', 
+                        'So much fun.', 
+                        'Definitly not sponsered to comment on this event.', 
+                        '7/10 too much water.', 
+                        'Saw too many birds for my liking.',
+                        'Too cold.', 
+                        'Organisers need to do a better job.',
+                        'I like wearing shorts they easy and comfy to wear',
+                        'Best spot ever',
+                        'We had a blast.',
+                        'Will definitely be back!',
+                        'The kids loved it!',
+                        'So many rocks...',
+                        'Not enough shade.',
+                        'We got lost on the way back.',
+                        'Saw a black bear!',
+                        'It was too much for us at our age.',
+                        "It's best to go with friends."]
         
         return {
           userId: user.id, 
